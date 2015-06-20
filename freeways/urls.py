39d6c858c1eb4f349572332be4ed0ad25b08658b
Keith Dechant <keith.dechant@gmail.com>
2015-06-20 14:41:20 -0700

    Added initial views, templates, and URL routing for the front end

---
+++ freeways/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^city/(?P<city_id>[0-9]+)/$', views.api, name='api'),
+]
--- freeways/views.py
+++ freeways/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+
+from .models import RouteSegment, City
+
+# Create your views here.
+def index(request):
+    cities = City.objects.all()
+    return render(request, 'index.html', {'cities': cities})
+
+# API method to provide stats and route data for a city. Outputs JSON.
+def api(request, city_id):
+    return HttpResponse(json.dumps('Under construction'), content_type="application/json")
