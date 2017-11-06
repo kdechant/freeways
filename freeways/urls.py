1a50a5ea3f0b76d4e6fdf01eab4ba1567684efa2
Keith Dechant <keith.dechant@gmail.com>
2017-11-05 20:25:18 -0800

    Upgrade to Django 2.0 beta 1

--- freeways/urls.py
+++ freeways/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^city/(?P<city_id>[0-9]+)/$', views.api, name='api'),
+    url(r'^city/(?P<city_id>[0-9]+)$', views.api, name='api'),
 ]
