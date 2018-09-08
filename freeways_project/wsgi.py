7f24fde9bf411bab01cc8469a11152963901f6f5
Keith Dechant <keith.dechant@gmail.com>
2018-09-08 01:16:55 -0700

    Upgrade Django to v2.1.1 (plus other packages) and install Pipenv

--- freeways_project/wsgi.py
+++ freeways_project/wsgi.py
@@ -16,17 +16,8 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append('/var/www/vhosts/freeways')
-sys.path.append('/var/www/vhosts/freeways/venv/lib/python3.4/site-packages')
+sys.path.append('/var/www/vhosts/freeways/venv/lib/python3.5/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freeways_project.settings")
 
-try:
-    application = get_wsgi_application()
-    print('WSGI without exception')
-except Exception:
-    print('handling WSGI exception')
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
+application = get_wsgi_application()
